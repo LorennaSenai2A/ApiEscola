21f3d49c24ed511705b6a148e10f5f6c0a861ebd
Aluna.Lorena <lorennasenaia@gmail.com>
2025-04-15 16:11:32 -0300

    Adicionada as models

--- ApiEscola/Data/APIEscolaContext.cs
+++ ApiEscola/Data/APIEscolaContext.cs
@@ -1,23 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using APIEscola.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace APIEscola.Data
 {
-    public class APIEscolaContext : IdentityDbContext //Herança do IdentityDbContext para incluir autenticação
+    public class APIEscolaContext : IdentityDbContext // Herança do IdentityDbContext para autenticação
     {
-        //Construtot que recebe as opções de configuração do DbContext
+        // Construtor que recebe as opções de configuração do DbContext
         public APIEscolaContext(DbContextOptions<APIEscolaContext> options) : base(options)
-        {
-        }
-        //Sobrecarrega o método OnModelCreating para configurar o modelo a partir da IdentityDbContext
+        { }
 
+        // Propriedade DbSet para cada tabela 
+        public DbSet<Aluno> Alunos { get; set; } // Tabela de Alunos
+        public DbSet<Curso> Cursos { get; set; } // Tabela de Cursos
+        public DbSet<Turma> Turmas { get; set; } // Tabela de Turmas
+        public DbSet<Matricula> Matriculas { get; set; } // Tabela de Matriculas
+
+        // Sobrecarga do método OnModelCreating para configurar o modelo a partir da IdentityDbContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Chama o método OnModelCreating da classe base para a criação das tabelas padrão
             base.OnModelCreating(modelBuilder);
+            // Configurar a criação de tabelas adicionais aqui
+            modelBuilder.Entity<Aluno>().ToTable("Alunos"); // Define o nome da tabela para Alunos
+            modelBuilder.Entity<Curso>().ToTable("Cursos");
+            modelBuilder.Entity<Turma>().ToTable("Turmas");
+            modelBuilder.Entity<Matricula>().ToTable("Matriculas");
 
-            //Configurar a criação de tabela de adicionais 
         }
     }
 }
-
